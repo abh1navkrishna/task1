dc5f1b352ddc8bcf00da4fa133889d9c5785ed1f
abh1navkrishna <abhinavkrishnamannarath@gmail.com>
2023-10-17 19:18:58 +0530

    add

---
+++ lib/task2.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class Task2 extends StatefulWidget {
+  const Task2({super.key});
+
+  @override
+  State<Task2> createState() => _Task2State();
+}
+
+class _Task2State extends State<Task2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent[100],
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 260,
+                width: 392,
+                color: Colors.deepOrangeAccent[100],
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Sign In',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w900, fontSize: 30)),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.file_upload_outlined)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          //-------------scnd cont--------------------------
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                child: Container(
+                  width: 360,
+                  height: 505,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30, top: 60),
+                        child: Row(
+                          children: [
+                            Icon(Icons.person_outline_rounded, size: 30),
+                            Text(
+                              ' Name',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 300,
+                              height: 50,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30, top: 30),
+                        child: Row(
+                          children: [
+                            Icon(Icons.email_outlined, size: 30),
+                            Text(
+                              ' Email',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 300,
+                              height: 50,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30, top: 30),
+                        child: Row(
+                          children: [
+                            Icon(Icons.lock_outline, size: 30),
+                            Text(
+                              ' Password',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 300,
+                              height: 50,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, left: 80),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 200,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.black, width: 1.0),borderRadius: BorderRadius.circular(10)
+                              ),
+                              child: Center(
+                                  child: Text('Done',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w900))),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
